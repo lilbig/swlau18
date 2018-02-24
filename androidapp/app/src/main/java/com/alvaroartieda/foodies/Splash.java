a5b512d97926ae3655c4f20b55e68edf7c619890
lilbig <radu.ionescu@gamaya.com>
2018-02-24 13:11:48 +0000

    ADDED: basic map view

--- androidapp/app/src/main/java/com/alvaroartieda/foodies/Splash.java
+++ androidapp/app/src/main/java/com/alvaroartieda/foodies/Splash.java
@@ -2,13 +2,12 @@ package com.alvaroartieda.foodies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.alvaroartieda.foodies.R;
 
 public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        //setContentView(R.layout.activity_splash);
     }
 }
